a3666da10eb3c8469da5b7082e9246a3b684fc7e
mathias-kinninkpo <kinninkpomathias@gmail.com>
2023-03-30 15:52:58 -0700

    new commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * _strcmp - compare 2 strings.
+ * @s1: string to be compared.
+ * @s2: string for comparation.
+ * Return: Difference.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1,s2));
+}
