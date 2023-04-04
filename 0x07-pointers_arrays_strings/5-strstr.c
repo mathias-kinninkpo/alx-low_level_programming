1d480c25a385721eeda49d7e37b32d65f4d91925
mathias-kinninkpo <kinninkpomathias@gmail.com>
2023-04-03 19:42:13 -0700

    new commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 /**
  * _strstr -  a function that locates a substring.
  * @haystack: an input string to search in
@@ -9,21 +9,5 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	char *startn = needle, *starth = haystack;
-
-	while (*haystack)
-	{
-		starth = haystack;
-		needle = startn;
-		while (*haystack == *needle)
-		{
-			haystack++;
-			needle++;
-		}
-
-		if (*needle == '\0')
-			return (haystack);
-		haystack = starth + 1;
-	}
-	return (NULL);
+	return (strstr(haystack, needle));
 }
