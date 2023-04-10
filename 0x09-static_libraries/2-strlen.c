58bf4885004fa67d31c41128685806e57cc676b1
mathias-kinninkpo <kinninkpomathias@gmail.com>
2023-04-10 15:13:19 -0700

    new and first commit for all

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+	c = strlen(s);
+	return (c);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * _puts - Prints a string
+  * @str: The string to print
+  *
+  * Return: void
+  */
+void _puts(char *str)
+{
+	puts(str);
+}
