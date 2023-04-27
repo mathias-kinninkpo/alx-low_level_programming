63328c0b9731457c1cad7692df9bbd794ca04fdc
mathias-kinninkpo <kinninkpomathias@gmail.com>
2023-04-27 14:49:38 -0700

    fixing betty error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
+
 /**
 * print_list - Prints linked list
 *
@@ -11,18 +12,17 @@
 size_t print_list(const list_t *h)
 {
 	unsigned int index = 0;
-	const list_t* list = NULL;
+	const list_t *list  = NULL;
+
 	list = h;
-	while(list != NULL)
+	while (list != NULL)
 	{
-		if(list->str == NULL)
+		if (list->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
-		{
 			printf("[%d] %s\n", list->len, list->str);
-		}
 		list = list->next;
 		index++;
 	}
